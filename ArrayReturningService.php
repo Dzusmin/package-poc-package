56b4510217a6da0ca8fe1240c0347870c26d74d9
Jakub Jasiński (Dzusmin) <dzusmin@gmail.com>
2022-09-11 21:21:18 +0200

    INIT

---
+++ ArrayReturningService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dzusmin\POCPackage;
+
+class ArrayReturningService
+{
+    public function returnArray(array $array): array
+    {
+        return $array;
+    }
+}
---
+++ Tests/ArrayReturningServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dzusmin\POCPackage\Tests;
+
+use Dzusmin\POCPackage\ArrayReturningService;
+use PHPUnit\Framework\TestCase;
+
+class ArrayReturningServiceTest extends TestCase
+{
+    /** @test */
+    public function it_should_return_same_array(): void
+    {
+        $subject = new ArrayReturningService();
+        $array = [];
+
+        $newArray = $subject->returnArray($array);
+
+        $this->assertSame($array, $newArray);
+    }
+}
